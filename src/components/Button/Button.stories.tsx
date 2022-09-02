c5b377b1dca20db6765108901c6b34b1e8b3a808
jacksonvfo <jacksonvfo@gmail.com>
2022-09-01 21:17:53 -0300

    refactor: button reusability and genericity improved

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,32 +1,44 @@
 // Button.stories.ts|tsx
 
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { MdAddCircleOutline } from "react-icons/md";
 
-import Button from './';
-import styles from './button.module.css'
+import Button from "./";
 
 export default {
   /* 👇 The title prop is optional.
-  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-  * to learn how to generate automatic titles
-  */
-  title: 'Button',
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Button",
   component: Button,
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-export const Secundary = Template.bind({});
+export const Add = Template.bind({});
+export const Default = Template.bind({});
+export const Rounded = Template.bind({});
 
-Primary.args = {
-  buttonStyle: 'primary',
-  children: 'Primary',
-  className: `${styles.vanillaButton} ${styles.primaryButton}`
+Add.args = {
+  borderRadius: "none",
+  buttonPadding: "medium",
+  children: (
+    <>
+      <MdAddCircleOutline size={18}/> Add Item
+    </>
+  ),
+  onClick: () => alert("Item added")
 };
 
-Secundary.args = {
-  buttonStyle: 'secundary',
-  children: 'Secundary',
-  className: `${styles.vanillaButton} ${styles.secundaryButton}`
+
+Default.args = {
+  onClick: () => alert("Default Clicked")
+};
+
+Rounded.args = {
+  borderRadius: "large",
+  buttonPadding: "large",
+  children: "Rounded button",
+  onClick: () => alert("Rounded Clicked")
 };
